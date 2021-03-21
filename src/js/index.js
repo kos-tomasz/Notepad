08914e800add3d9efe8f95c9f5e6d89cc3c99ad6
Tomasz Kos <kos.tomasz.mateusz@gmail.com>
2021-03-21 23:07:17 +0100

    feat: dark/light mode added

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,21 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
 // registerSW();
 
-/* place your code below */
+console.log("HELLO 🚀");
 
-console.log('HELLO 🚀')
+let isDark = true;
+const button = document.querySelector(".header__isDark--js");
+
+button.addEventListener("click", () => {
+  if (isDark) {
+    document.documentElement.style.setProperty("--background-color", "#bc9ef5");
+    isDark = false;
+  } else {
+    document.documentElement.style.setProperty("--background-color", "#2D2241");
+    isDark = true;
+  }
+});
+
