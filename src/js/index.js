d6e9bd612cbf504ba4bc3df11ec2180e993e3636
Tomasz Kos <kos.tomasz.mateusz@gmail.com>
2021-03-23 12:36:02 +0100

    feat: media queries & localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,24 @@ import "../scss/main.scss";
 
 console.log("HELLO 🚀");
 
-let isDark = true;
-const button = document.querySelector(".header__isDark--js");
 
-button.addEventListener("click", () => {
+const save = document.querySelector('.save--js');
+const load = document.querySelector('.load--js');
+const textarea = document.querySelector('.form__text--js');
+
+save.addEventListener('click', () =>{
+localStorage.setItem('entry', textarea.value);
+});
+
+load.addEventListener('click', () =>{
+   textarea.value = localStorage.getItem('entry');
+});
+
+
+let isDark = true;
+const darkMode = document.querySelector('.header__isDark--js');
+
+darkMode.addEventListener('click', () => {
   if (isDark) {
     document.documentElement.style.setProperty("--background-color", "#bc9ef5");
     isDark = false;
@@ -18,4 +32,3 @@ button.addEventListener("click", () => {
     isDark = true;
   }
 });
-
